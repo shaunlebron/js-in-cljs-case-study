ad6897fd36f4fba5a33ae1cbb0966b0c11977cb1
Shaun Williams <shaun.e.williams@gmail.com>
2017-10-24 17:08:35 -0500

    update parinfer-codemirror to resolve parinfer transitive dep properly

--- src/my/app.cljs
+++ src/my/app.cljs
@@ -1,11 +1,7 @@
 (ns my.app
   (:require
     ["codemirror" :as codemirror]
-    ["parinfer" :as parinfer]
     ["parinfer-codemirror" :as parinfer-codemirror]))
-
-;; until I setup parinfer-codemirror to properly depend on parinfer
-(set! js/parinfer parinfer)
 
 (let [cm (codemirror js/document.body)]
   (parinfer-codemirror/init cm))
